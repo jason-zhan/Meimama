aaf3ebe9c3fb809a777772c5c587446feca5f80b
Shengmin Zhan <zhanshengmin2010@gmail.com>
2015-02-27 15:52:33 -0600

    initial

---
+++ sourceCode/Android/MyAppWeixin/src/cn/buaa/myweixin/Login.java
@@ -0,0 +1,71 @@
+package cn.buaa.myweixin;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends Activity {
+	private EditText mUser; // ’ ∫≈±‡º≠øÚ
+	private EditText mPassword; // √‹¬Î±‡º≠øÚ
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        
+        mUser = (EditText)findViewById(R.id.login_user_edit);
+        mPassword = (EditText)findViewById(R.id.login_passwd_edit);
+        
+    }
+
+    public void login_mainweixin(View v) {
+    	if("buaa".equals(mUser.getText().toString()) && "123".equals(mPassword.getText().toString()))   //≈–∂œ ’ ∫≈∫Õ√‹¬Î
+        {
+             Intent intent = new Intent();
+             intent.setClass(Login.this,LoadingActivity.class);
+             startActivity(intent);
+             //Toast.makeText(getApplicationContext(), "µ«¬º≥…π¶", Toast.LENGTH_SHORT).show();
+          }
+        else if("".equals(mUser.getText().toString()) || "".equals(mPassword.getText().toString()))   //≈–∂œ ’ ∫≈∫Õ√‹¬Î
+        {
+        	new AlertDialog.Builder(Login.this)
+			.setIcon(getResources().getDrawable(R.drawable.login_error_icon))
+			.setTitle("Login")
+			.setMessage("User Name or Password is empty.")
+			.create().show();
+         }
+        else{
+           
+        	new AlertDialog.Builder(Login.this)
+			.setIcon(getResources().getDrawable(R.drawable.login_error_icon))
+			.setTitle("µ«¬º ß∞‹")
+			.setMessage("Œ¢–≈’ ∫≈ªÚ’ﬂ√‹¬Î≤ª’˝»∑£¨\n«ÎºÏ≤È∫Û÷ÿ–¬ ‰»Î£°")
+			.create().show();
+        }
+    	
+    	//µ«¬º∞¥≈•
+    	/*
+      	Intent intent = new Intent();
+		intent.setClass(Login.this,Whatsnew.class);
+		startActivity(intent);
+		Toast.makeText(getApplicationContext(), "µ«¬º≥…π¶", Toast.LENGTH_SHORT).show();
+		this.finish();*/
+      }  
+    public void login_back(View v) {     //±ÍÃ‚¿∏ ∑µªÿ∞¥≈•
+      	this.finish();
+      }  
+    public void login_pw(View v) {     //Õ¸º«√‹¬Î∞¥≈•
+    	Uri uri = Uri.parse("http://3g.qq.com"); 
+    	Intent intent = new Intent(Intent.ACTION_VIEW, uri); 
+    	startActivity(intent);
+    	//Intent intent = new Intent();
+    	//intent.setClass(Login.this,Whatsnew.class);
+        //startActivity(intent);
+      }  
+}
